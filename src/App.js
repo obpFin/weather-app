9a202b6fc06be10e9ac625eaf35ca3eb119699d5
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-06-15 20:36:18 +0300

    Initial state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+  const INITIAL_STATE = function() {
+    kelvin: 0,
+    city: Helsinki,
+    windspeed: 0,
+    humidity: 0, 
+    showKelvins: true 
+  }
+
+  const Coordinates = React.createClass({
+    getCoordinates:function() {
+      if ("geolocation" in navigator) {
+
+        function success(position) {
+          var latitude = position.coords.latitude;
+          var longitude = position.coords.longitude;
+          return {latitude,longitude};
+        }
+
+        function error() {
+          console.log("Unable to retrieve your location");
+        }
+
+        return navigator.geolocation.getCurrentPosition(success, error); 
+      } else {
+        console.log("geolocation is not available");
+      }    
+    },
+
+    render:function(){
+
+    }  
+  });
+
 class App extends Component {
+
   render() {
     return (
       <div id="app">
@@ -11,7 +45,7 @@ class App extends Component {
         <p id="city" class="thick"></p>
         <img id="weatherIcon" src="" />
         <p id="celsius" class="celsius thick"></p>
-        <button class="toggle">convert to Fahrenheit</button>
+        <button class="toggle">Convert to Fahrenheit</button>
         <div class="text">
           <p id="desc" class="thick"></p>
         </div>
