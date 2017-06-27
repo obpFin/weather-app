ca32ec17962beb99c39427815868b4a6248a277f
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-06-27 20:45:29 +0300

    get weather with axios

--- src/App.js
+++ src/App.js
@@ -1,93 +1,102 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Axios from 'axios';
-import logo from './logo.svg';
-import { myConfig } from './config.js';
+import myConfig from './config.js';
 import './App.css';
 
 
 
-  const INITIAL_STATE = {
-    kelvin: 0,
-    city: 'Helsinki',
-    windspeed: 0,
-    humidity: 0, 
-    showKelvins: true 
-  }
+const INITIAL_STATE = {
+	kelvin: 0,
+	city: 'Helsinki',
+	windspeed: 0,
+	humidity: 0, 
+	showKelvins: true 
+}
 
-  class Coordinates extends React.Component{
+class Coordinates extends React.Component{
 
-    constructor (props) {
-      super();
-      this.state = {
-        latitude:0, 
-        longitude:0
-      };
-    }
+	constructor (props) {
+		super();
+		this.state = {
+			latitude:0, 
+			longitude:0
+		};
+	}
 
-    getCoordinates() {
-        Axios.get('https://freegeoip.net/json/', {
-          method: 'get'
-        }).then(response => {
-          var position = response.data;
-          this.setState({latitude:position.latitude,longitude:position.longitude});
-        }).catch(function(error){
-            console.log("error getting coordinates",error)
-        }); 
-    }
+	getCoordinates() {
+			Axios.get('https://freegeoip.net/json/', {
+				method: 'get'
+			}).then(response => {
+				var position = response.data;
+				this.setState({latitude:position.latitude,longitude:position.longitude});
+			}).catch(error => {
+					console.log("error getting coordinates",error)
+			}); 
+	}
 
-    getInitalstate() {
-      return INITIAL_STATE;
-    }
+	getInitalstate() {
+		return INITIAL_STATE;
+	}
 
-    componentDidMount() {
-      var position = this.getCoordinates();
-      fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + this.state.latitude +
-      "&lon=" + this.state.longitude + "&APPID="+myConfig.apiUrl)
-        .then(response => response.json())
-        .then(responseData => {
-          this.setState({weather:responseData});
-        })
-        .catch(error => {
-          console.log('Error getting weather data',error);
-        });
-    }
+	componentDidMount() {
+		Axios.get('https://freegeoip.net/json/', {
+				method: 'get'
+			}).then(response => {
+				var position = response.data;
+				this.setState({latitude:position.latitude,longitude:position.longitude});
+			}).then( () => {
+				Axios.get("http://api.openweathermap.org/data/2.5/weather?lat=" + this.state.latitude +
+				"&lon=" + this.state.longitude + "&APPID="+myConfig.apiUrl, {
+				method: 'get'
+			}).then(response => {
+				var weather = response.data;
+				console.log(weather);
+			})
+			}).catch(error => {
+				console.log("error getting weather",error)
+			});	
+	}
 
-    render(){
-      console.log(this.state.latitude);
-      console.log(this.state.longitude);
-      return null;
-    }  
-  }
+	render(){
+		console.log(this.state.latitude);
+		console.log(this.state.longitude);
+		return (
+			<div>
+				<h2>Weather App</h2>
+				<p>{this.state.latitude}</p>
+				<p>{this.state.longitude}</p>
+			</div>
+		);
+	}  
+}
 
-  class Weather extends React.Component {
-    getInitalstate(){
-      return INITIAL_STATE;
-    }
-  };
+class Weather extends React.Component {
+	getInitalstate(){
+		return INITIAL_STATE;
+	}
+};
 
 class App extends React.Component {
 
-  render() {
-    return (
-      <div id="app">
-        <Coordinates />
-        <div id="title">
-        </div>
-        <p id="city" className="thick"></p>
-        <p id="celsius" className="celsius thick"></p>
-        <button className="toggle">Convert to Fahrenheit</button>
-        <div className="text">
-          <p id="desc" className="thick"></p>
-        </div>
-        <div className="text">
-          <p id="wind" className="thick"></p>
-        </div>
-        <div className="text">
-          <p id="hum" className="thick"></p>
-        </div>
-      </div>  
-    );   
-  }
+	render() {
+		return (
+			<div id="app">
+				<Coordinates />
+				<p id="city" className="thick"></p>
+				<p id="celsius" className="celsius thick"></p>
+				<button className="toggle">Convert to Fahrenheit</button>
+				<div className="text">
+					<p id="desc" className="thick"></p>
+				</div>
+				<div className="text">
+					<p id="wind" className="thick"></p>
+				</div>
+				<div className="text">
+					<p id="hum" className="thick"></p>
+				</div>
+			</div>  
+		);   
+	}
 }
 
 export default App;
