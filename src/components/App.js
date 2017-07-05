d146f629191c666685a78605f592f50e80b3fc58
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-07-05 22:05:04 +0300

    UI tweaks, period selection

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../App.css';
 import Weather from './Weather.js'
 
+
+
 class App extends React.Component {
 
 	constructor (props) {
@@ -18,24 +20,9 @@ class App extends React.Component {
 	render() {
 
 		return (
-			<div id="app">
+			<div className="container" id="app">
 				<div>
-					<h2>Weather App</h2>
-					<p>{this.state.latitude}</p>
-					<p>{this.state.longitude}</p>
-				</div>
 				<Weather />
-				<p id="city" className="thick"></p>
-				<p id="celsius" className="celsius thick"></p>
-				<button className="toggle">Convert to Fahrenheit</button>
-				<div className="text">
-					<p id="desc" className="thick"></p>
-				</div>
-				<div className="text">
-					<p id="wind" className="thick"></p>
-				</div>
-				<div className="text">
-					<p id="hum" className="thick"></p>
 				</div>
 			</div>  
 		);   
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,21 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Api from '../utils/Api.js';
 
+function SelectPeriod(props) {
+
+	var periods = ['Day','Hour','Summary'];
+
+	return (
+		<ul className='periods'>
+			{periods.map(function(period) {
+				return (
+					<li 
+						style={period === props.selectedPeriod ? {color: '#000000'} : null}
+						onClick={props.onSelect.bind(null, period)}
+						key={period}>
+						{period}
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
+
+SelectPeriod.propTypes = {
+	selectedPeriod: PropTypes.string.isRequired,
+	onSelect: PropTypes.func.isRequired,
+}
+
 class Weather extends React.Component {
 
 	constructor (props) {
 		super();
 		this.state = {
-			latitude: 0, 
-			longitude: 0,
-			weather: null
+			city:null,
+			weather: null,
+			selectedPeriod:'Day'
 		};
+		this.updatePeriod = this.updatePeriod.bind(this);
+
 	}
 
 	componentDidMount(){
+		this.updatePeriod(this.state.selectedPeriod);
+
 		Api.fetchCoordinates()
 			.then(res => {
-				console.log('res',res);
+				this.setState({
+					city:res.city
+				})
 				Api.fetchDarkSkyWeather(res.latitude,res.longitude)
 					.then(res => {
 						this.setState({
@@ -26,12 +57,26 @@ class Weather extends React.Component {
 			});
 	}
 
+	updatePeriod(period) {
+		this.setState({
+				selectedPeriod: period,
+				//weather:null
+		});
+	}
+
 	render() {
 		return (
 			<div>
+				<SelectPeriod selectedPeriod={this.state.selectedPeriod}
+				onSelect={this.updatePeriod}
+				/>
 				{!this.state.weather
 				 ? <p>Loading</p>
-				 : <p>{this.state.weather.daily.summary}</p>}	
+				 : <div>
+				 	<p className='city'>{this.state.city}</p>
+				 	<p className='degrees'>{this.state.weather.currently.temperature} 'C</p>
+				 	<p>{this.state.weather.daily.summary}</p>
+				   </div>}
 			</div>
 
 		);
