6ecd8d3b17736d4ab6b7572ca0ebe9e56717f126
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-07-11 19:14:46 +0300

    weatherDetail logic

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../App.css';
 import Nav from './Nav.js'
 import WeatherSummary from './WeatherSummary.js'
+import WeatherDetail from './WeatherDetail.js'
+
 
 
 
@@ -9,13 +11,13 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<div className="container" id="app">
+			<div className="container top" id="app">
 				<div className='main-content'>
 				<Nav />
 				<WeatherSummary />
 				</div>
 				<div className='footer'>
-
+					<WeatherDetail />
 				</div>
 			</div>  
 		);   
--- src/components/WeatherDetail.js
+++ src/components/WeatherDetail.js
@@ -3,47 +3,42 @@ import PropTypes from 'prop-types';
 import Api from '../utils/Api.js';
 import Skycons from 'react-skycons';
 
-class WeatherDetail extends React.Component {
+class WeatherDetail extends React.Component {	
 
 	constructor (props) {
 		super();
 		this.state = {
-			city:null,
 			weather: null
 		};
-		this.updatePeriod = this.updatePeriod.bind(this);
 
 	}
 
 	componentDidMount(){
-		this.updatePeriod(this.state.selectedPeriod);
 
 		Api.fetchGoogleApiCoordinates()
 			.then(res => {
-				Api.fetchGoogleGeocodingLocation(res.location.lat,res.location.lng)
-					.then(res => {
-						this.setState({city:Api.getCityFromLocationResponse(res)})
-					});
+				console.log('location',res);
 				Api.fetchDarkSkyWeather(res.location.lat,res.location.lng)
-					.then(res => {
-						this.setState({weather: res})			
-					})
-				})	
+			.then(res => {
+				console.log('weather');
+				this.setState({weather: res})			
+			})
+			})	
+			
 	}
+		
+
 
 	render() {
 		return (
 			<div>
-				<SelectPeriod selectedPeriod={this.state.selectedPeriod}
-				onSelect={this.updatePeriod}
-				/>
 				{!this.state.weather
 				 ? <p>Loading</p>
 				 : <div>
-				 	<p className='city'>{this.state.city}</p>
-				 	<Skycons className='icons' color='white' icon={this.state.weather.currently.icon.toUpperCase().replace(/-/g,'_')} autoplay={true}/>
-				 	<p className='degrees'>{Math.round(this.state.weather.currently.temperature)}&#x00B0;C</p>
-				 	<p className='daily-summary'>{this.state.weather.currently.summary}</p>
+				 	<tr className='row'>
+				 		<td>Cloudcover: {(this.state.weather.currently.cloudCover * 100) + '%'}</td>
+				 	</tr>
+				 	<p className=''>{this.state.weather.currently.summary}</p>
 				   </div>}
 			</div>
 		);
