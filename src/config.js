4149dd97b57f206b1c3d9fbde8caa3fcc7a26ffc
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-07-19 22:44:29 +0300

    deploy app

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const MyConfig = { 
+    darkSky: '928a432a1c9095ad106d89861c81a4da',
+    googleLocation:'AIzaSyBbcLZaVylOA8f9opdpOXM7Vd3zcyqJc_k',
+    googleGeocoding:'AIzaSyCzNNvLZBq22V6JfifBQrkCsm0cxNJ62vE' 
+};
+
+module.exports = MyConfig;
+
+
